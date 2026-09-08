529589d1e9519bc11e65ad1466319cbd5038bc81
Hitury <xxhituryxx@gmail.com>
2026-09-08 12:45:03 +0200

    Portfolio navbar changes

--- src/components/TSX/Home/header.tsx
+++ src/components/TSX/Home/header.tsx
@@ -1,7 +1,17 @@
 export default function Header() {
     return (
-        <header className="p-5 flex justify-center">
-            <h1>Portfolio</h1>
+        <header className="p-5 flex justify-between">
+            <div className="flex bg-raised/40 justify-center flex-row gap-7 border-solid border-line border py-3 px-7 rounded-2xl">
+                <a className="text-text hover:text-amber transition-colors duration-250 cursor-pointer">Home</a>
+            </div>
+            <div className="flex bg-raised/40 justify-center flex-row gap-7 border-solid border-line border py-3 px-7 rounded-2xl">
+                <a className="text-text hover:text-amber transition-colors duration-250 cursor-pointer">Projects</a>
+                <a className="text-text hover:text-amber transition-colors duration-250 cursor-pointer">Skills</a>
+                <a className="text-text hover:text-amber transition-colors duration-250 cursor-pointer">Experience</a>
+            </div>
+            <div className="flex bg-raised/40 justify-center flex-row gap-7 border-solid border-line border py-3 px-7 rounded-2xl">
+                <a className="text-text hover:text-amber transition-colors duration-250 cursor-pointer">Contact</a>
+            </div>
         </header>
     )
 }
